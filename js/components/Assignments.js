c9759cd934eb1fcbc99089dd24770e92047ffeca
Simon Wong <simonwong1985hk@gmail.com>
2022-07-25 11:43:06 +0800

    parent child state communication

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,19 +1,15 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 
 export default {
-  components: { AssignmentList },
+  components: { AssignmentList, AssignmentCreate },
 
   template: `
     <section class="space-y-6">
       <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
       <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
 
-      <form @submit.prevent="add">
-        <div class="border border-gray-600 text-black">
-          <input v-model="newAssignment" placeholder="New assignment..." class="p-2" />
-          <button type="submit" class="bg-white p-2 border-l">Add</button>
-        <div>
-      </form>
+      <assignment-create @add="add"></assignment-create>
     </section>
   `,
 
@@ -24,8 +20,6 @@ export default {
         { id: 2, title: "Assignment 2", completed: false },
         { id: 3, title: "Assignment 3", completed: false },
       ],
-
-      newAssignment: "",
     };
   },
 
@@ -43,14 +37,12 @@ export default {
   },
 
   methods: {
-    add() {
+    add(title) {
       this.assignments.push({
         id: this.assignments.length + 1,
-        title: this.newAssignment,
+        title: title,
         completed: false,
       });
-
-      this.newAssignment = "";
     },
   },
 };
