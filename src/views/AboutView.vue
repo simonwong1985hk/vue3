c92d7e5e7c6d1060ebcf0373aaa1a81887824379
Simon Wong <simonwong1985hk@gmail.com>
2022-07-28 09:47:53 +0800

    mixins

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,15 @@
+<script>
+import flash from "@/mixins/flash";
+
+export default {
+  mixins: [flash],
+};
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <p>
+      <button @click="flash('It Works On The About Page!')">Click Me</button>
+    </p>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup>
-import TheWelcome from "@/components/TheWelcome.vue";
+<script>
+import flash from "@/mixins/flash";
+
+export default {
+  mixins: [flash],
+};
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p>
+      <button @click="flash('It Works!')">Click Me</button>
+    </p>
   </main>
 </template>
