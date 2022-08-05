c1fc1479f3a18ebcda4bf027ff76df4f74b1b537
Simon Wong <simonwong1985hk@gmail.com>
2022-08-05 08:52:06 +0800

    build a modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,56 @@
+<script setup>
+defineProps({
+  show: Boolean,
+});
+</script>
+
+<template>
+  <div v-if="show" class="modal-mask">
+    <div class="modal-container">
+      <div>
+        <slot>default body</slot>
+      </div>
+
+      <footer class="modal-footer">
+        <slot name="footer">
+          <button @click="$emit('close')">Close</button>
+        </slot>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal-mask {
+  position: fixed;
+  inset: 0;
+  background: rgba(0, 0, 0, 0.6);
+  display: grid;
+  place-items: center;
+}
+
+.modal-container {
+  background: white;
+  padding: 1rem;
+  width: 80vw;
+  max-width: 500px;
+  border-radius: 7px;
+}
+
+.modal-footer {
+  border-top: 1px solid #ddd;
+  margin-top: 1rem;
+  padding-top: 0.5rem;
+  font-size: 0.8rem;
+}
+
+.modal-footer button {
+  background: #ddd;
+  padding: 0.25rem 0.75rem;
+  border-radius: 20px;
+}
+
+.modal-footer button:hover {
+  background: #c8c8c8;
+}
+</style>
--- src/components/Teams/TeamHeader.vue
+++ src/components/Teams/TeamHeader.vue
@@ -10,6 +10,7 @@ let team = useTeamStore();
       <button
         class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
         :disabled="!team.spotsRemaining"
+        @click="$emit('add')"
       >
         Add Member ({{ team.spotsRemaining }} Spots Left)
       </button>
