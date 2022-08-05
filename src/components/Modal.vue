20ce00178d07c103a62069e4c54d616146bdc084
Simon Wong <simonwong1985hk@gmail.com>
2022-08-05 11:28:30 +0800

    transition component

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,19 +5,28 @@ defineProps({
 </script>
 
 <template>
-  <div v-if="show" class="modal-mask">
-    <div class="modal-container">
-      <div>
-        <slot>default body</slot>
-      </div>
+  <Transition
+    enter-from-class="opacity-0 scale-125"
+    enter-to-class="opacity-100 scale-100"
+    enter-active-class="transition duration-300"
+    leave-from-class="opacity-100 scale-100"
+    leave-to-class="opacity-0 scale-125"
+    leave-active-class="transition duration-200"
+  >
+    <div v-if="show" class="modal-mask">
+      <div class="modal-container">
+        <div>
+          <slot>default body</slot>
+        </div>
 
-      <footer class="modal-footer">
-        <slot name="footer">
-          <button @click="$emit('close')">Close</button>
-        </slot>
-      </footer>
+        <footer class="modal-footer">
+          <slot name="footer">
+            <button @click="$emit('close')">Close</button>
+          </slot>
+        </footer>
+      </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <style>
